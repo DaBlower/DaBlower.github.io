8a31669280c132880a9d6a41a41a1eefab84076e
DaBlower <113282214+DaBlower@users.noreply.github.com>
2023-03-19 07:08:14 +1100

    Update main.js
    mobile edit (39:34)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,18 @@
 // Grab elements
-
+const selectElement = selector => {
+    const element = document.querySelector(selector)
+    if(element) return element;
+    throw new Error('Something went wrong, make sure that ${selector} exists or is typed correctly.');
+};
 //Nav styles on scroll
-
+const scrollHeader = () => {
+    const headerElement = selectElement('#header');
+    if(this.scrolly >= 15){
+        headerElement.classList.add('activated')
+    }else{
+        
+    }
+};
 // Open menu & search pop-up
 
 // Open/Close search form popup
